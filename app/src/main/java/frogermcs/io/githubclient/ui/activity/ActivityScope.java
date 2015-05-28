944406b39b87d681d5230262867cb517bc6b80d9
Mirek Stanek <froger.mcs@gmail.com>
2015-05-28 09:10:57 +0200

    cleanup

--- app/src/main/java/frogermcs/io/githubclient/ui/activity/ActivityScope.java
+++ app/src/main/java/frogermcs/io/githubclient/ui/activity/ActivityScope.java
@@ -1,14 +1,10 @@
 package frogermcs.io.githubclient.ui.activity;
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
 import javax.inject.Scope;
 
 /**
  * Created by Miroslaw Stanek on 22.04.15.
  */
 @Scope
-@Retention(RetentionPolicy.RUNTIME)
 public @interface ActivityScope {
 }
